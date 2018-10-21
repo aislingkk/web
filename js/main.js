b0ce97eb193858cb2899b40b850e6a59a945feea
sand <shmsand@gmail.com>
2018-10-21 12:18:16 -0700

    update

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $("rect").hover( function(event) {
     let index = $(event.currentTarget).index() + 1;
     $("#rectboxlabel").find("text").removeClass("rectbox-36-show");
-    // $("#rectboxlabel:nth-child(" + index + ")").addClass("rectbox-36-show");
 
+    // change this part tot find the text related to the rectbox
     $("#rectboxlabel")
       .children(":nth-child(" + index + ")")
       .addClass("rectbox-36-show");
     $("#rectboxlabel")
-      .children(":nth-child(" + index * 2 + 1 + ")")
+      .children(":nth-child(" + index + 1 + ")")
       .addClass("rectbox-36-show");
   });
 });
