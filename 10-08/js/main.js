2a63a270f487c340ea0cf0ba84199997e976b482
Bao YiFang <byf.aisling@gmail.com>
2018-10-21 20:10:39 +0200

    add file

---
+++ 10-08/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".bubble").on("click", function (event) {
+        let index = $(event.currentTarget).index() + 1;
+        $(".label").removeClass("label--shown")
+
+        $(".label:nth-child(" + index + ")").addClass("label--shown");
+    })
+})
