645e1779e9e572ba5d96fa42596311a0043cf43e
sand <shmsand@gmail.com>
2018-10-21 12:11:22 -0700

    Fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("rect").on("click", function(event) {
+    $("rect").hover( function(event) {
     let index = $(event.currentTarget).index() + 1;
     $("#rectboxlabel").find("text").removeClass("rectbox-36-show");
     // $("#rectboxlabel:nth-child(" + index + ")").addClass("rectbox-36-show");
@@ -10,7 +10,5 @@ $(document).ready(function() {
     $("#rectboxlabel")
       .children(":nth-child(" + index * 2 + 1 + ")")
       .addClass("rectbox-36-show");
-
-    console.log("#rectboxlabel:nth-child(" + index + ")");
   });
 });
