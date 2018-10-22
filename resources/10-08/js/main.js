3c96b7ccff7f28e7140efe92f9f262762bb6d332
sand <shmsand@gmail.com>
2018-10-21 22:14:08 -0700

    update

--- resources/10-08/js/main.js
+++ resources/10-08/js/main.js
@@ -1,8 +1,10 @@
-$(document).ready(function () {
-    $(".bubble").on("click", function (event) {
-        let index = $(event.currentTarget).index() + 1;
-        $(".label").removeClass("label--shown")
+$(document).ready(function() {
+  $(".bubble").on("click", function(event) {
+    let index = $(event.currentTarget).index() + 1;
+    $(".label").removeClass("label--shown");
 
-        $(".label:nth-child(" + index + ")").addClass("label--shown");
-    })
-})
+    $(".label:nth-child(" + index + ")").addClass("label--shown");
+  });
+
+
+});
